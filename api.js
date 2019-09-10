49139bd0f33b51a7467e01d7fbbd808fdbf60acf
Jerome <jerome@spinprotocol.io>
2019-09-10 16:47:00 +0900

    Fetch the data by using axios

--- api.js
+++ api.js
@@ -8,17 +8,13 @@ const api = axios.create({
   },
 });
 
-export const MoviesApi = {
-  nowPlaying: () => api.get('movie/now_playing'),
-  upcoming: () => api.get('movie/upoming'),
-  popular: () => api.get('movie/popular'),
-  movieDetail: id =>
-    api.get(`movie/${id}`, {
-      params: {
-        append_to_response: 'videos',
-      },
-    }),
-  search: term =>
+export const movies = {
+  getMovie: id =>
+    api.get(`movie/${id}`, { params: { append_to_response: 'videos' } }),
+  getPopular: () => api.get('movie/popular'),
+  getUpcoming: () => api.get('movie/upcoming'),
+  getNowPlaying: () => api.get('movie/now_playing'),
+  searchMovies: term =>
     api.get('search/movie', {
       params: {
         query: encodeURIComponent(term),
@@ -26,17 +22,13 @@ export const MoviesApi = {
     }),
 };
 
-export const TVApi = {
-  topRated: () => api.get('tv/top_rated'),
-  popular: () => api.get('tv/popular'),
-  airingToday: () => api.get('tv/airing_today'),
-  showDetail: id =>
-    api.get(`tv/${id}`, {
-      params: {
-        append_to_response: 'videos',
-      },
-    }),
-  search: term =>
+export const tv = {
+  getShow: id =>
+    api.get(`tv/${id}`, { params: { append_to_response: 'videos' } }),
+  getPopular: () => api.get('tv/popular'),
+  getTopRated: () => api.get('tv/top_rated'),
+  getAiringToday: () => api.get('tv/airing_today'),
+  searchTv: term =>
     api.get('search/tv', {
       params: {
         query: encodeURIComponent(term),
--- screens/Movies/MoviesContainer.js
+++ screens/Movies/MoviesContainer.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import MoviesPresenter from './MoviesPresenter';
+import { movies } from '../../api';
 
 export default class MoviesContainer extends React.Component {
   state = {
     loading: true,
+    upcoming: null,
+    popular: null,
+    nowPlaying: null,
+    error: null,
   };
+
+  async componentDidMount() {
+    try {
+      const upcoming = await movies.getUpcoming();
+      const popular = await movies.getPopular();
+      const nowPlaying = await movies.getNowPlaying();
+    } catch (error) {
+      this.setState({ error: "Can't get Movies." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
 
   render() {
     const { loading } = this.state;
