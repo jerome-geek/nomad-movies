3d2497ff3917247c7910e384e5ad88ba787f414e
Jerome <jerome@spinprotocol.io>
2019-09-12 17:37:10 +0900

    Create Movie Swiper
     - Create Components: MovieSlider, MovieSlide, MoviePoster
     - Create utils: makePhotoUrl to return full path of Image

---
+++ components/MovieSlide.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import makePhotoUrl from '../utils/makePhotoUrl';
+import Layout from '../constants/Layout';
+import MoviePoster from './MoviePoster';
+
+const Container = styled.View`
+  flex: 1;
+  position: relative;
+`;
+
+const Content = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  padding-horizontal: 30px;
+  justify-content: space-between;
+`;
+
+const BgImage = styled.Image`
+  width: ${Layout.width};
+  height: ${Layout.height / 3};
+  opacity: 0.5;
+  position: absolute;
+`;
+
+const MovieSlide = ({
+  posterPhoto,
+  backgroundPhoto,
+  title,
+  voteAvg,
+  overview,
+}) => (
+  <Container>
+    <BgImage source={{ uri: makePhotoUrl(backgroundPhoto) }} />
+    <Content>
+      <MoviePoster path={posterPhoto} />
+    </Content>
+  </Container>
+);
+
+MovieSlide.propTypes = {
+  id: PropTypes.number.isRequired,
+  posterPhoto: PropTypes.string.isRequired,
+  backgroundPhoto: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  voteAvg: PropTypes.number.isRequired,
+  overview: PropTypes.string.isRequired,
+};
+
+export default MovieSlide;
