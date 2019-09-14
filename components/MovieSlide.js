2702a8df194e80df92e493847f96ff8db7b6b43a
Jerome <jerome@spinprotocol.io>
2019-09-14 14:03:10 +0900

    Add Vote Average, Overview, Button to MovieSlide Component

--- components/MovieSlide.js
+++ components/MovieSlide.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import MoviePoster from './MoviePoster';
+import MovieRating from './MovieRating';
 import makePhotoUrl from '../utils/makePhotoUrl';
 import Layout from '../constants/Layout';
-import MoviePoster from './MoviePoster';
+import { TINT_COLOR } from '../constants/Colors';
 
 const Container = styled.View`
   flex: 1;
   position: relative;
+`;
+
+const BgImage = styled.Image`
+  width: ${Layout.width};
+  height: ${Layout.height / 3};
+  opacity: 0.3;
+  position: absolute;
 `;
 
 const Content = styled.View`
@@ -18,11 +27,36 @@ const Content = styled.View`
   justify-content: space-between;
 `;
 
-const BgImage = styled.Image`
-  width: ${Layout.width};
-  height: ${Layout.height / 3};
-  opacity: 0.5;
-  position: absolute;
+const Column = styled.View`
+  width: 60%;
+  align-items: flex-start;
+`;
+
+const Title = styled.Text`
+  color: ${TINT_COLOR};
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const Overview = styled.Text`
+  color: ${TINT_COLOR};
+  font-size: 12px;
+  margin-bottom: 10px;
+`;
+
+const VoteContainer = styled.View`
+  margin: 10px 0px;
+`;
+
+const ButtonContainer = styled.TouchableOpacity`
+  background-color: #e74c3c;
+  padding: 5px;
+  border-radius: 2.5px;
+`;
+
+const ButtonText = styled.Text`
+  color: #fff;
+  font-size: 12px;
 `;
 
 const MovieSlide = ({
@@ -36,6 +70,24 @@ const MovieSlide = ({
     <BgImage source={{ uri: makePhotoUrl(backgroundPhoto) }} />
     <Content>
       <MoviePoster path={posterPhoto} />
+      <Column>
+        <Title>{title}</Title>
+        {voteAvg ? (
+          <VoteContainer>
+            <MovieRating votes={voteAvg} inSlide={true} />
+          </VoteContainer>
+        ) : null}
+        {overview ? (
+          <Overview>
+            {overview.length > 117
+              ? `${overview.substring(0, 120)}...`
+              : overview}
+          </Overview>
+        ) : null}
+        <ButtonContainer>
+          <ButtonText>More details</ButtonText>
+        </ButtonContainer>
+      </Column>
     </Content>
   </Container>
 );
