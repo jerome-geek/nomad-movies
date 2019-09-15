2810e1d9c3bd4b599cf6db4a4d292a7fab1601a3
Jerome <jerome@spinprotocol.io>
2019-09-15 15:14:46 +0900

    Add Section & MovieItem Components to Presenters

--- screens/TV/TVPresenter.js
+++ screens/TV/TVPresenter.js
@@ -1,22 +1,58 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Loader from '../../components/Loader';
+import Section from '../../components/Section';
+import MovieItem from '../../components/MovieItem';
+import { BG_COLOR } from '../../constants/Colors';
 
-const TvPresenter = ({ loading, popular, topRated, airingToday }) =>
+const Container = styled.ScrollView`
+  background-color: ${BG_COLOR};
+`;
+
+const TVPresenter = ({ loading, popular, topRated, airingToday }) =>
   loading ? (
     <Loader />
   ) : (
-    <View>
-      <Text>TV</Text>
-    </View>
+    <Container>
+      {airingToday ? (
+        <Section title="Airing Today">
+          {airingToday
+            .filter(tv => tv.poster_path !== null)
+            .map(tv => (
+              <MovieItem
+                key={tv.id}
+                id={tv.id}
+                posterPhoto={tv.poster_path}
+                title={tv.name}
+                voteAvg={tv.vote_average}
+              />
+            ))}
+        </Section>
+      ) : null}
+      {topRated ? (
+        <Section title="Top Rated">
+          {topRated
+            .filter(tv => tv.poster_path !== null)
+            .map(tv => (
+              <MovieItem
+                key={tv.id}
+                id={tv.id}
+                posterPhoto={tv.poster_path}
+                title={tv.name}
+                voteAvg={tv.vote_average}
+              />
+            ))}
+        </Section>
+      ) : null}
+    </Container>
   );
 
-TvPresenter.propTypes = {
+TVPresenter.propTypes = {
   loading: PropTypes.bool.isRequired,
   popular: PropTypes.array,
   topRated: PropTypes.array,
   airingToday: PropTypes.array,
 };
 
-export default TvPresenter;
+export default TVPresenter;
