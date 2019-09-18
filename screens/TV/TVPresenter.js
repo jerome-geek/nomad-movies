dcc06e925c0185a4a3d8c1e45d62a8f97641bf02
Jerome <jerome@spinprotocol.io>
2019-09-18 09:12:12 +0900

    Create Popular Section to Presenter

--- screens/TV/TVPresenter.js
+++ screens/TV/TVPresenter.js
@@ -31,8 +31,23 @@ const TVPresenter = ({ loading, popular, airingThisWeek, airingToday }) =>
         </Section>
       ) : null}
       {airingThisWeek ? (
-        <Section title="Top Rated">
+        <Section title="Airing this Week">
           {airingThisWeek
+            .filter(tv => tv.poster_path !== null)
+            .map(tv => (
+              <MovieItem
+                key={tv.id}
+                id={tv.id}
+                posterPhoto={tv.poster_path}
+                title={tv.name}
+                voteAvg={tv.vote_average}
+              />
+            ))}
+        </Section>
+      ) : null}
+      {popular ? (
+        <Section title="Popular">
+          {popular
             .filter(tv => tv.poster_path !== null)
             .map(tv => (
               <MovieItem
