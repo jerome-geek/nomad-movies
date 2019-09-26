8eac368a6dbbedeacec16d95434c42ad7e89f9d8
Jerome <jerome@spinprotocol.io>
2019-09-26 09:10:00 +0900

    Add Main Content & Linear Gradient to Detail Screen

--- screens/Detail/DetailPresenter.js
+++ screens/Detail/DetailPresenter.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { LinearGradient } from 'expo-linear-gradient';
 import MoviePoster from '../../components/MoviePoster';
+import MovieRating from '../../components/MovieRating';
 import { BG_COLOR, TINT_COLOR } from '../../constants/Colors';
 import Layout from '../../constants/Layout';
 import makePhotoUrl from '../../utils/makePhotoUrl';
@@ -17,7 +20,7 @@ const Header = styled.View`
 
 const BgImage = styled.Image`
   width: ${Layout.width};
-  height: ${Layout.height / 3};
+  height: ${Layout.height / 3.5};
   opacity: 0.3;
   position: absolute;
   top: 0;
@@ -27,8 +30,8 @@ const Content = styled.View`
   flex: 1;
   flex-direction: row;
   align-items: flex-end;
-  padding-horizontal: 30px;
-  height: ${Layout.height / 3};
+  padding-horizontal: 20px;
+  height: ${Layout.height / 3.5};
 `;
 
 const Column = styled.View`
@@ -37,8 +40,27 @@ const Column = styled.View`
 
 const Title = styled.Text`
   color: ${TINT_COLOR};
-  font-size: 14px;
+  font-size: 18px;
   font-weight: 600;
+  margin-bottom: 10px;
+`;
+
+const MainContent = styled.View`
+  padding-horizontal: 20px;
+  margin-top: 25px;
+  width: 80%;
+`;
+
+const ContentTitle = styled.Text`
+  color: ${TINT_COLOR};
+  font-weight: 600;
+  margin-bottom: 10px;
+`;
+
+const Overview = styled.Text`
+  color: ${TINT_COLOR};
+  font-size: 12px;
+  margin-bottom: 10px;
 `;
 
 const DetailPresenter = ({
@@ -52,13 +74,33 @@ const DetailPresenter = ({
   <Container>
     <Header>
       <BgImage source={{ uri: makePhotoUrl(backgroundPhoto) }} />
-      <Content>
-        <MoviePoster path={posterPhoto} />
-        <Column>
-          <Title>{title}</Title>
-        </Column>
-      </Content>
+      <LinearGradient
+        colors={['transparent', 'black']}
+        start={Platform.select({
+          ios: [0, 0],
+        })}
+        end={Platform.select({
+          ios: [0, 0.5],
+          android: [0, 0.9],
+        })}
+      >
+        <Content>
+          <MoviePoster path={posterPhoto} />
+          <Column>
+            <Title>{title}</Title>
+            <MovieRating inSlide={true} votes={voteAvg} />
+          </Column>
+        </Content>
+      </LinearGradient>
     </Header>
+    <MainContent>
+      {overview ? (
+        <React.Fragment>
+          <ContentTitle>Overview</ContentTitle>
+          <Overview>{overview}</Overview>
+        </React.Fragment>
+      ) : null}
+    </MainContent>
   </Container>
 );
 
